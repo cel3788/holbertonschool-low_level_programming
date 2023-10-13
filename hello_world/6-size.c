07470857a561eab04879b7df3730b3f6bbf3fe2f
cel3788 <7576@holbertonstudents.com>
2023-10-13 14:45:51 -0700

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <inttypes.h>
 
-int main(void) {
+int main(void)
+{
+
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
