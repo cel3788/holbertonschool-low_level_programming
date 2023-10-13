9368509a77c4141348e16a50bd62252dec893edf
cel3788 <7576@holbertonstudents.com>
2023-10-13 16:20:26 -0700

    101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,12 +6,13 @@ int main(void)
     const char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     ssize_t len = strlen(quote);
 
-if (write(2, quote, len) != len)
-{
-// Handle write error
-return (1);
+    if (write(2, quote, len) != len)
+    {
+        // Handle write error
+        return (1);
+    }
+
+    return (1);
 }
 
-return (1);
-}
 
