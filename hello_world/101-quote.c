03fc46ee4823b69bcd4daf6226c4256915ae908a
cel3788 <7576@holbertonstudents.com>
2023-10-14 01:54:48 -0700

    101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,19 +1,14 @@
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-    const char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = strlen(quote);
-
-    if (write(2, quote, len) != len)
-    {
-        perror("Error writing to standard error");
-        return (1);
-    }
-
+    write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",58);
     return (1);
 }
 
