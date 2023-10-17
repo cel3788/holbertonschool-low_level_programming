03f436db705eaa0f9d9fcd33d31efd673af6e821
Your Name <you@example.com>
2023-10-17 16:07:05 -0700

    main.c

--- makefiles/main.c
+++ makefiles/main.c
@@ -1,12 +1,9 @@
-#include "m.h"
+#include <stdio.h>
+#include <stdlib.h> // Include for EXIT_SUCCESS
 
-/**
- * main - Entry point
- *
- * Return: Always 
- */
-int main(void)
-{
-	print_school();
-	return (EXIT_SUCCESS);
+// Function declarations or other code
+
+int main(void) {
+    // Your main function code
+    return EXIT_SUCCESS;
 }
