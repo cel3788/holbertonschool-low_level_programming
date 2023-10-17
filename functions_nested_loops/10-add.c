162e322e6593a73b5180a336a56f428fcddc000d
Your Name <you@example.com>
2023-10-17 13:58:44 -0700

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Sum of two integers
+ * @n0: First number
+ * @n1: Second number
+ * Return: Sum of both variables
+ */
+int add(int n0, int n1)
+{
+	return (n0 + n1);
+}
