9c93b80bfd41d68654f059676524c688f0ba1583
Your Name <you@example.com>
2023-10-17 07:48:23 -0700

    100

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,37 +1,31 @@
-/*
- * File: 10-print_comb2.c
- * Auth: Brennan D Baraban
- */
-
+#include "holberton.h"
 #include <stdio.h>
 
 /**
- * main - Prints the numbers from 00 to 99, numbers separated by
- *        a comma followed by a space, in ascending order.
- *
- * Return: Always 0.
+ * print_to_98 - Prints natural numbers to 98
+ * @n: Number being evaluated
  */
-int main(void)
+void print_to_98(int n)
 {
-	int digit1, digit2;
-
-	for (digit1 = 0; digit1 < 10; digit1++)
+	if (n <= 98)
 	{
-		for (digit2 = 0; digit2 < 10; digit2++)
+		while (n <= 98)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 9 && digit2 == 9)
-			continue;
-			
-			putchar(',');
-			putchar(' ');
+			printf("%i", n);
+			if (n != 98)
+				printf(", ");
+			n++;
 		}
 	}
-
-	putchar('\n');
-
-	return (0);
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%i", n);
+			if (n != 98)
+				printf(", ");
+			n--;
+		}
+	}
+	printf("\n");
 }
-
