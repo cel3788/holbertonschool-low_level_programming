7d76a92f9b8bcb4d90fadb0f919859687b95e6b4
Your Name <you@example.com>
2023-10-17 13:36:12 -0700

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - Prints out absolute value
+ * @r: Number to be evaluated
+ * Return: Given value in absolute form
+ */
+int _abs(int r)
+{
+	if (r < 0)
+		r *= -1;
+	return (r);
+}
