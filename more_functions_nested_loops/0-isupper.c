7205431b3fcb748569d4d467c578676037cd7c55
Your Name <you@example.com>
2023-10-19 10:14:31 -0700

    0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+*_isupper function that checks for uppercase character
+*@c: input charavter
+*Returns: 1 if c is uppercase or 0 otherwise 
+*/
+int _isupper(int c)
+{
+    char uppercase = 'A';
+
+    for (; uppercase <= 'Z'; uppercase++)
+    {
+        if (c == uppercase)
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
