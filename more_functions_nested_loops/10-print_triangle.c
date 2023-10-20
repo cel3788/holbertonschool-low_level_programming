3d83d5246c6d06e04a6d62bd9ed9e4a31408326c
Your Name <you@example.com>
2023-10-20 08:54:35 -0700

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,26 +6,25 @@
  */
 void print_triangle(int size)
 {
-    int row, space, hash;
+	int row, space, hash;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        for (row = 0; row < size; row++)
-        {
-            for (space = 0; space < size - row - 1; space++)
-            {
-                _putchar(' ');
-            }
-            for (hash = 0; hash < row + 1; hash++)
-            {
-                _putchar('#');
-            }
-            _putchar('\n');
-        }
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (space = 0; space < size - row - 1; space++)
+			{
+				_putchar(' ');
+			}
+			for (hash = 0; hash < row + 1; hash++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
-
