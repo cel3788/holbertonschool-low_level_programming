1d8d80d0a5d9f3885975c55cab78f52965e88a74
Your Name <you@example.com>
2023-10-27 04:00:15 -0700

    6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,39 @@
 #include "main.h"
-#include <string.h> // Include the string.h header for strchr function
-#include <stddef.h> // Include this for NULL definition
 
 /**
- * cap_string - Capitalizes the first letter of each word in a string.
+ * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
  *
  * Return: A pointer to the changed string.
  */
-char *cap_string(char *str) {
-    int index = 0;
-    int capitalizeNext = 1; // Flag to indicate if the next letter should be capitalized
+char *cap_string(char *str)
+{
+	int index = 0;
 
-    while (str[index]) {
-        if (str[index] >= 'a' && str[index] <= 'z') {
-            if (capitalizeNext) {
-                str[index] -= 32; // Capitalize the letter
-                capitalizeNext = 0; // Reset the flag
-            }
-        } else {
-            // Check for word separators
-            if (strchr(" \t\n,;.!?\"(){}", str[index]) != NULL) {
-                capitalizeNext = 1; // Set the flag to capitalize the next letter
-            }
-        }
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
 
-        index++;
-    }
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
 
-    return str;
+		index++;
+	}
+
+	return (str);
 }
 
