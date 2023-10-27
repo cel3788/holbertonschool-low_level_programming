2f17f42dd42a97a42be65fa84c8f3d322a58c76b
Your Name <you@example.com>
2023-10-27 06:13:48 -0700

    7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,24 +1,36 @@
 #include "main.h"
-
 /**
- * leet - Transforms a string into 1337 (leet) speak.
- * @str: The string to transform.
+ *leet - encodes a string into 1337.
+ *letters a and A are replaced by 4.
+ *Letters e and E are replaced by 3
+ *Letters o and O are replaced by 0
+ *Letters t and T are replaced by 7
+ *Letters l and L are replaced by 1
+ *@s: pointer to string.
  *
- * Return: A pointer to the transformed string.
+ *Return: pointer to s.
  */
-char *leet(char *str)
+char *leet(char *s)
 {
-    /* Implementation of the leet transformation here */
-    return str;
+	int stringCount, leetCount;
+	char leetLetters[] = "aAeEoOtTlL";
+	char leetNums[] = "4433007711";
+
+	/*  scan through string */
+	stringCount = 0;
+	while (s[stringCount] != '\0')
+		/* check whether leetLetter is found */
+	{
+		leetCount = 0;
+		while (leetCount < 10)
+		{
+			if (leetLetters[leetCount] == s[stringCount])
+			{
+				s[stringCount] = leetNums[leetCount];
+			}
+			leetCount++;
+		}
+		stringCount++;
+	}
+	return (s);
 }
-
-int main(void)
-{
-    char str[] = "Hello, world!";
-    leet(str); // Call the leet function on the string
-
-    /* Rest of your main function code here */
-
-    return (0);
-}
-
