ddf41e4e828c2f57f83be3dc885f1db03a2f3f11
Your Name <you@example.com>
2023-10-27 02:18:38 -0700

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,28 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes all words in a string.
+ * cap_string - Capitalizes the first letter of each word in a string.
  * @str: The string to be capitalized.
  *
  * Return: A pointer to the changed string.
  */
 char *cap_string(char *str) {
     int index = 0;
+    int capitalizeNext = 1; // Flag to indicate if the next letter should be capitalized
 
     while (str[index]) {
-        // Check if the current character is a lowercase letter
         if (str[index] >= 'a' && str[index] <= 'z') {
-            // Capitalize the letter
-            str[index] -= 32;
+            if (capitalizeNext) {
+                str[index] -= 32; // Capitalize the letter
+                capitalizeNext = 0; // Reset the flag
+            }
+        } else {
+            // Check for word separators
+            if (strchr(" \t\n,;.!?\"(){}", str[index]) != NULL) {
+                capitalizeNext = 1; // Set the flag to capitalize the next letter
+            }
         }
 
-        // Move to the next character
         index++;
     }
 
