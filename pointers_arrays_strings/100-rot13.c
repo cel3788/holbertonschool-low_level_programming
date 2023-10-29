b6960359e1f07c2e42562bc06e7251b294a3b6f2
Your Name <you@example.com>
2023-10-29 14:54:15 -0700

    100-rot13.c

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,32 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * rot13 - rotate characters 13 places in the alphabet
  * @s: string
  * Return: string `s` rotated
  */
-
 char *rot13(char *s)
 {
     int i;
-    char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-    char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
     for (i = 0; s[i] != '\0'; i++)
     {
-        char *position = strchr(alphabet, s[i]);
-        if (position != NULL)
+        char c = s[i];
+        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
         {
-            int index = position - alphabet;
-            s[i] = storeh[index];
+            char base = (c >= 'A' && c <= 'Z') ? 'A' : 'a';
+            s[i] = (c - base + 13) % 26 + base;
         }
     }
     return s;
+}
+
+int main()
+{
+    char input[] = "Hello, World!";
+    printf("Original: %s\n", input);
+    rot13(input);
+    printf("ROT13 Encoded: %s\n", input);
+    return 0;
 }
 
