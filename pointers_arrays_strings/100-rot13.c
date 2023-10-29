0fbb4fb86235020ac05efd68c082c8ce6c23309b
Your Name <you@example.com>
2023-10-29 14:42:50 -0700

    100-rot13.c

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * rot13 - rotate characters 13 places in the alphabet
  * @s: string
@@ -8,17 +7,21 @@
 
 char *rot13(char *s)
 {
-	int i;
-	char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char storel[] = "nopqrstuvwxyzabcdefghijklm";
+    int i;
+    char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+    char storel[] = "nopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
-		{
-			s[i] = (s[i] - 65 > 25) ?
-				storel[s[i] - 97] : storeh[s[i] - 65];
-		}
-	}
-	return (s);
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if ((s[i] >= 'A' && s[i] <= 'Z'))
+        {
+            s[i] = storeh[s[i] - 'A'];
+        }
+        else if ((s[i] >= 'a' && s[i] <= 'z'))
+        {
+            s[i] = storel[s[i] - 'a'];
+        }
+    }
+    return (s);
 }
+
