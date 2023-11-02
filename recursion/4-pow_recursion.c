24a6d9e183b072083f4a33b9d42b5ce631323edd
Your Name <you@example.com>
2023-11-01 18:04:04 -0700

    4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -5,11 +5,14 @@
  * @x: Number Integer
  * @y: Power
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Result of x raised to the power of y
  */
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
-		return (-1);
+    if (y < 0)
+        return (-1); // Error condition for negative exponent
+    if (y == 0)
+        return (1);  // Anything raised to the power of 0 is 1
+    return (x * _pow_recursion(x, y - 1)); // Recursive calculation of power
 }
+
