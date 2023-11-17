4c0eccd89871b2026d8e68ff6f7e45635950441a
cel3788 <7576@holbertonstudents.com>
2023-11-17 10:58:12 -0800

    100-hello_holberton.asm

--- variadic_functions/100-hello_holberton.asm
+++ variadic_functions/100-hello_holberton.asm
@@ -1,23 +1,17 @@
-%define sys_write 1
-%define stdout 1
-
-%define sys_exit 60
-%define success 0
-
-%define nl 10
-
-section .data
-
-	message db "Hello, Holberton", nl
+global main
 
 section .text
-	global main
 main:
-	mov rax, sys_write
-	mov rdi, stdout
-	mov rsi, message
-	mov rdx, 17
-	syscall
-	mov    rax, sys_exit
-	mov    rdi, success
-	syscall
+	mov rax, 1; write(
+	mov rdi, 1; STDOUT_FILENO,
+	mov rsi, msg; "Hello, Holberton\n",
+	mov rdx, msglen ; sizeof("Hello, Holberton\n")
+	syscall ; );
+
+	mov rax, 60 ; exit(
+	mov rdi, 0; EXIT_SUCCESS
+	syscall ; );
+
+section .data
+	msg: db "Hello, Holberton", 10
+	msglen: equ $ - msgl
