8907a5cfaa5c4894aa0ee5ff8484f237ec976c4c
cel3788 <7576@holbertonstudents.com>
2023-11-29 02:57:19 -0800

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,63 +1,65 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_idx - Insert a new node into a doubly linked list at a given index
+ * insert_dnodeint_at_index - Insert a new node into a doubly linked list at a given index
  * @head: Double pointer to the start of the list
  * @idx: Index to insert new node at
  * @n: Value to assign to new node
  * Return: Address of the new node, NULL if it fails
  */
-dlistint_t *insert_dnodeint_at_idx(dlistint_t **head, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
 {
-	unsigned int pos;
-	dlistint_t *new_node, *current, *prev;
+    unsigned int pos;
+    dlistint_t *new_node, *current, *prev;
 
-	if (*head == NULL && idx != 0)
-		return (NULL);
+    if (!head)
+        return (NULL);
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
+    new_node = malloc(sizeof(dlistint_t));
+    if (!new_node)
+        return (NULL);
 
-	if (*head != NULL)
-	{
-		prev = NULL;
-		current = *head;
+    if (*head != NULL)
+    {
+        prev = NULL;
+        current = *head;
 
-		// Check if idx is greater than the length of the list
-		if (idx > 0)
-		{
-			for (pos = 0; pos < idx && current != NULL; pos++)
-			{
-				prev = current;
-				current = current->next;
-			}
-		}
+        // Check if idx is greater than the length of the list
+        if (idx > 0)
+        {
+            for (pos = 0; pos < idx && current != NULL; pos++)
+            {
+                prev = current;
+                current = current->next;
+            }
+        }
 
-		if (pos == idx)
-		{
-			new_node->n = n;
-			new_node->prev = prev;
+        if (pos == idx)
+        {
+            new_node->n = n;
+            new_node->prev = prev;
 
-			if (current != NULL)
-				current->prev = new_node;
+            if (current != NULL)
+                current->prev = new_node;
 
-			new_node->next = current;
+            new_node->next = current;
 
-			if (idx != 0)
-				prev->next = new_node;
-			else
-				*head = new_node;
+            if (idx != 0)
+                prev->next = new_node;
+            else
+                *head = new_node;
 
-			return (new_node);
-		}
-		return (NULL);
-	}
+            return (new_node);
+        }
+        free(new_node);  // Free the allocated memory if insertion fails
+        return (NULL);
+    }
 
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	new_node->n = n;
-	*head = new_node;
-	return (new_node);
+    // Handling the case when the list is empty
+    new_node->next = NULL;
+    new_node->prev = NULL;
+    new_node->n = n;
+    *head = new_node;
+    return (new_node);
 }
 
