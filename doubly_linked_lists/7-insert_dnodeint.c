5221c02bb91021502c15afc69659593b037f587c
cel3788 <7576@holbertonstudents.com>
2023-11-29 02:41:10 -0800

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,43 +1,51 @@
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position.
- * @h: double pointer to list.
- * @idx: index of inserting position.
- * @n: data for new node insertion.
- * Return: the address of the new node.
+ * insert_dnodeint_at_index - insert node at index
+ * @h: reference to list
+ * @idx: index of the new node
+ * @n: value in list
+ * Return: node added
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i = 0;
-	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *tmp_node = *h;
+	dlistint_t *node; /* new node */
+	dlistint_t *runner; /* runner in the list */
+	unsigned int r_idx = 0; /* runner index */
 
-	if (new_node)
+	if (h) /* check if it's a valid list*/
 	{
-		new_node->n = n;
-		if (idx == 0)
+		node = malloc(sizeof(*node)); /* check if node has memory */
+		if (node)
 		{
-			if (*h)
+			node->prev = NULL;
+			node->next = NULL;
+			node->n = n;
+			for (runner = *h; r_idx < idx; r_idx++)
 			{
-				new_node->next = *h;
-				(*h)->prev = new_node;
+				runner = runner->next;
+				/* Check if length of list < idx*/
+				if (!runner)
+				{
+					free(node);
+					return (NULL); /* ERROR */
+				}
 			}
-			*h = new_node;
-			return (new_node);
+			if (runner == NULL)
+			{
+				*h = node;
+				return (node);
+			}
+			node->next = runner->next; /* runner->next */
+			node->prev = runner;
+			runner->next = node;
+			if (node->next) /* check if runner is not the last node */
+			{
+				runner = node->next;
+				runner->prev = node;
+			}
+			return (node);
 		}
-		while (i < idx - 1 && tmp_node)
-		{
-			tmp_node = tmp_node->next;
-			i++;
-		}
-		if (tmp_node)
-		{
-			if (tmp_node->next)
-				tmp_node->next->prev = new_node;
-			new_node->next = tmp_node->next;
-			new_node->prev = tmp_node;
-			tmp_node->next = new_node;
-			return (new_node);
-		}
+		return (NULL); /* ERROR */
 	}
-	return (NULL);
+	return (NULL); /* ERROR */
 }
