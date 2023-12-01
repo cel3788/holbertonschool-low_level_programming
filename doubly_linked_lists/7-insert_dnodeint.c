edcaa849464fc746c188c6f83bc3e9dc1a6c8bdf
cel3788 <7576@holbertonstudents.com>
2023-12-01 08:59:37 -0800

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,26 +1,43 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index - get the node
- * @head: head
- * @index: index
- * Return: 0
+ * insert_dnodeint_at_index - inserts a new node at a given position.
+ * @h: double pointer to list.
+ * @idx: index of inserting position.
+ * @n: data for new node insertion.
+ * Return: the address of the new node.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int count = 0;
+	unsigned int i = 0;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *tmp_node = *h;
 
-	while (count < idx)
+	if (new_node)
 	{
-
-		if ((*h)->next == NULL)
+		new_node->n = n;
+		if (idx == 0)
 		{
-			return (NULL);
+			if (*h)
+			{
+				new_node->next = *h;
+				(*h)->prev = new_node;
+			}
+			*h = new_node;
+			return (new_node);
 		}
-		else
+		while (i < idx - 1 && tmp_node)
 		{
-			count++;
-			*h =(*h)->next;
+			tmp_node = tmp_node->next;
+			i++;
+		}
+		if (tmp_node)
+		{
+			if (tmp_node->next)
+				tmp_node->next->prev = new_node;
+			new_node->next = tmp_node->next;
+			new_node->prev = tmp_node;
+			tmp_node->next = new_node;
+			return (new_node);
 		}
 	}
-	return (*h);
+	return (NULL);
 }
